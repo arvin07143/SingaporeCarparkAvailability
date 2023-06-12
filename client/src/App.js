33a6d5041cc20e8f61cf2dd8f13454830fea86ab
Arvin Ng <69192027+arvin07143@users.noreply.github.com>
2023-06-12 07:08:46 +0000

    this should work

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
 
   const fetchData = () => {
-    axios.get("http://host.docker.internal:4000/")
+    axios.get("http://localhost:4000/")
     .then((response) => {
       setData(response.data)
     })
